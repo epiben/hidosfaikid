967341aa7a7bb7b0674ba240f121fdcc9795780e
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2021-10-08 23:09:42 +0200

    Add missing file

---
+++ scripts/01-fetch_valid_egfr.sql
@@ -0,0 +1,10 @@
+WITH cte_egfr AS (
+    SELECT
+        pid as person_id
+        , shown_clean AS egfr
+        , drawn_datetime as egfr_datetime
+    FROM @schema.@table_name
+    WHERE clean_quantity_id IN ('DNK35131', 'DNK35301', 'DNK35302')
+        AND drawn_datetime BETWEEN '2006-01-01 00:00:00' AND '2016-06-30 23:59:59'
+)
+SELECT * from cte_egfr WHERE egfr IS NOT NULL;
